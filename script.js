33fdc1030ee69671f75630fd8f30adc8d023dfa2
skills254 <skillslab254@gmail.com>
2026-01-29 13:53:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Accordion Function
+var acc = document.getElementsByClassName("accordion");
+for (var i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+// Contact Form Alert
+document.getElementById("contactForm").addEventListener("submit", function(e){
+    e.preventDefault();
+    alert("Thank you! Your message has been received.");
+});
